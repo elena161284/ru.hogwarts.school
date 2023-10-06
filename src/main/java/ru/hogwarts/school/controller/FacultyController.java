654714b5ffd969b0e30d5ecebf708a6488371a7f
Elena <elena161284@mail.ru>
2023-10-06 18:23:15 +0300

    commit ru/hogwarts/school3

---
+++ src/main/java/ru/hogwarts/school/controller/FacultyController.java
@@ -0,0 +1,36 @@
+package ru.hogwarts.school.controller;
+import org.springframework.web.bind.annotation.*;
+import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.service.FacultyService;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/faculty")
+public class FacultyController {
+    private final FacultyService facultyService;
+
+    public FacultyController(FacultyService facultyService) {
+        this.facultyService = facultyService;
+    }
+    @GetMapping("{id}")
+    public Faculty getInfo(@PathVariable long id) {
+        return facultyService.findFaculty(id);
+    }
+    @PostMapping
+    public Faculty createFaculty(@RequestBody Faculty faculty){
+        return facultyService.createFaculty(faculty);
+    }
+    @PutMapping()
+    public Faculty editFaculty(@RequestBody Faculty faculty) {
+        return facultyService.editFaculty(faculty.getId(), faculty);
+    }
+    @DeleteMapping("{id}")
+    public Faculty deletedFaculty(@PathVariable Long id) {
+        return facultyService.deletedFaculty(id);
+    }
+    @GetMapping("/color")
+    public Collection<Faculty> byColor(@RequestParam String color) {
+        return facultyService.filterByColor(color);
+    }
+}
